8ce51e8e228affa16bfa8952228d59311f8fb88c
Dimon <bolilui.dmytro@gmail.com>
2023-08-16 15:08:55 +0300

    Add check if new phone number in use or not

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -32,7 +32,7 @@ router.post('/register', validateBody(registerSchema), ctrl.register);
 router.post('/login', validateBody(loginSchema), ctrl.login);
 router.post('/logout', authenticate, ctrl.logout);
 router.get('/current', authenticate, ctrl.getCurrentUser);
-router.patch(
+router.put(
   '/current/update',
   authenticate,
   validateBody(updateSchema),
--- schemas/users/registerSchema.js
+++ schemas/users/registerSchema.js
@@ -4,7 +4,7 @@ const registerSchema = Joi.object({
   // name: Joi.string().required(),
   email: Joi.string().required(),
   password: Joi.string().required(),
-  userType: Joi.string().valid('patient', 'doctor'),
+  // userType: Joi.string().valid('patient', 'doctor'),
 });
 
 module.exports = registerSchema;
